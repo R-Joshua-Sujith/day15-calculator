c07bd7c53761ad8b5881ba434949af62fdb05a6c
R-Joshua-Sujith <joshuasujith14@gmail.com>
2023-05-31 11:11:53 +0530

    completed

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 let string = "";
 let buttons = document.querySelectorAll('.button');
+let numbers = document.querySelectorAll('.numbers');
+
+
+window.addEventListener('keydown', (e) => {
+    let keycode = e.which ? e.which : e.keycode;
+    if (keycode >= 48 && keycode <= 57) {
+        console.log(e.key)
+        string = string + e.key;
+        document.querySelector('input').value = string;
+    }
+    else {
+        alert("only numbers are allowed")
+    }
+})
+
+
+
 Array.from(buttons).forEach((button) => {
     button.addEventListener('click', (e) => {
         if (e.target.innerHTML == '=') {
@@ -11,8 +28,30 @@ Array.from(buttons).forEach((button) => {
             document.querySelector('input').value = string;
         }
         else {
+            console.log(e.target)
             string = string + e.target.innerHTML;
             document.querySelector('input').value = string;
         }
     })
 })
+
+
+
+
+
+
+
+
+// Array.from(buttons).forEach((button) => {
+//     button.addEventListener('keyup', (e) => {
+//         let keycode = e.which ? e.which : e.keycode;
+//         if (keycode >= 48 && keycode <= 57) {
+//             console.log(e.target)
+//             string = string + e.target.value;
+//             document.querySelector('input').value = string;
+//         }
+//         else {
+//             alert("only numbers are allowed")
+//         }
+//     })
+// })
